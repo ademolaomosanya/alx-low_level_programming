e676595ca9a43cfab51c45e50598dc22d73d4037
Omosanya Ademola <omosanyaademola@gmail.com>
2022-06-08 14:43:33 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,23 @@ int _putchar(char c)
   
 {
   
-  return (write(1, &c, 1));
+  _putchar('H');
+  
+  _putchar('o');
+  
+  _putchar('l');
+  
+  _putchar('b');
+  
+  _putchar('e');
+  
+  _putchar('r');
+  
+  _putchar('t');
+  
+  _putchar('o');
+  
+  _putchar('n');
+_putchar('\n');
   
 }

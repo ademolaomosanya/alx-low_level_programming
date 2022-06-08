1d8a9978d54d2d7d475b445cf25d783d4dce4f5b
Omosanya Ademola <omosanyaademola@gmail.com>
2022-06-08 15:02:40 -0700

    msg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c);
+int _putchar(char c)
 char c;
 {
-  c =" _putchar"
-  _putchar(c);
-  _putchar('\n');
-  return(0);
+c =" _putchar"
+_putchar(c);
+_putchar('\n');
+return(0);
 }

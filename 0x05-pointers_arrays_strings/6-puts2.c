a2ca22cb5e5dc6402f3c1a836c37e8d03d520e2f
Omosanya Ademola <omosanyaademola@gmail.com>
2022-06-13 10:18:38 -0700

    put2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,36 @@
 #include "main.h"
 /**
- * puts2 - main function.
- * @str: Pointer, the string to print.
- *
- * Description: This function prints every other character of a string
- * (divisible by two)
- * Return: none.
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-void puts2(char *str)
+int _strlen(char *s)
 {
-int i;
-while (str[i])
+int len = 0;
+while (*s != '\0')
 {
-if (i % 2 == 0)
-{
-_putchar(str[i]);
+len++;
+s++;
 }
-i++;
+return (len);
+}
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+int idx;
+int len = _strlen(str);
+/* find the index to start depending on even/odd amount of strlen */
+if (len % 2 != 0)
+idx = (len / 2) + 1;
+else
+idx = (len / 2);
+while (idx < len)
+{
+_putchar(*(str + idx));
+idx++;
 }
 _putchar('\n');
 }

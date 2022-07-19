e0a82d70fac10e00cf2cc0788f426ea0eeacce02
Omosanya Ademola <omosanyaademola@gmail.com>
2022-07-19 16:22:04 -0700

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,47 +1,22 @@
 #include "lists.h"
-
-
-
 /**
-
  * sum_listint - Function that sum lists
-
  * @head: head nodo
-
  *
-
  * Return: suma
-
  */
-
 int sum_listint(listint_t *head)
-  
 {
-  
-  int sum = 0;
-  
-
-  
-  if (head != NULL)
-    
-    {
-      
-      while (head->next != NULL)
-	
-	{
-	  
-	  sum += head->n;
-	  
-	  head = head->next;
-	  
-	}
-      
-      sum += head->n;
-      
-      return (sum);
-      
-    }
-  
-  return (sum);
-  
+int sum = 0;
+if (head != NULL)
+{
+while (head->next != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+sum += head->n;
+return (sum);
+}
+return (sum);
 }

89103237b0cf055b270175b68faa61f2d5cd8157
Omosanya Ademola <omosanyaademola@gmail.com>
2022-07-25 17:08:36 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,89 @@
-#include <stdio.h>
 #include "holberton.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+
+
+
 /**
- * read_textfile - Reads a text file and prints it to the POSIX
- * standard output.
- * @filename: file.
- * @letters: Number of letters it should read and print.
- * Return: Actual number of letters it could read and print.
+
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+
+ * @filename: pointer to the file name
+
+ * @letters: the number of letters it should read and print
+
+ * Return:  the actual number of letters it could read and print
+
  */
+
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
+  
 {
-int fd, res_read, res_write;
-char *buf;
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(sizeof(char) * letters);
-if (buf == NULL)
-return (0);
-res_read = read(fd, buf, letters);
-if (res_read == -1)
-{
-free(buf);
-return (0);
-}
-res_write = write(STDOUT_FILENO, buf, res_read);
-if (res_write == -1 || res_read != res_write)
-{
-free(buf);
-return (0);
-}
-free(buf);
-close(fd);
-return (res_write);
+  
+
+  
+  int file;
+  
+  ssize_t rcount, wcount;
+  
+  char *buffer;
+  
+
+  
+  if (filename == NULL)
+    
+    return (0);
+  
+
+  
+  file = open(filename, O_RDWR);
+  
+  if (file == -1)
+    
+    return (0);
+  
+
+  
+  buffer = malloc(sizeof(char) * letters);
+  
+  if (buffer == NULL)
+    
+    {
+      
+      free(buffer);
+      
+      return (0);
+      
+    }
+  
+
+  
+  rcount = read(file, buffer, letters);
+  
+  if (rcount == -1)
+    
+    return (0);
+  
+
+  
+  wcount = write(STDOUT_FILENO, buffer, rcount);
+  
+
+  
+  if (wcount == -1 || rcount != wcount)
+    
+    return (0);
+  
+
+  
+  free(buffer);
+  
+
+  
+  close(file);
+  
+
+  
+  return (wcount);
+  
 }
